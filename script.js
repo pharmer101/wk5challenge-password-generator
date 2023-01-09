d5879d090edb702eab712e30f7dafb7c3a2e6ee3
pharmer101 <117358586+pharmer101@users.noreply.github.com>
2023-01-09 16:06:55 +0000

    add main functions
    Upload manually as issue with github connection

--- script.js
+++ script.js
@@ -88,20 +88,105 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
+// This is the array to use to generate the random characters for the password.  New options (such as special characters) are added to this if the user clicks "ok" when promtpted if they want to include the option in  their generated password 
+includedArrays = []
+var psLength  //!! *** HARD CODED FOR TEST - REMOVE NUMBER***
+var Newpass 
+
+// FUNCTION 1 -----------------------------------------------Function to prompt user for password options --------------------------------------------------------------------------------------------------------------------------------------
 function getPasswordOptions() {
 
-}
+  psLength = prompt("Testing - How many Characters do you want your password to be(betweeen 10 and 64");
+  if (psLength.length ){
+    if(psLength < 10 || psLength  > 64 || psLength === " "){
+      pslength = prompt("That is not a valid password length - please pick a number between 10 and 64")
+  }}
+  console.log(psLength) // Check to seee if user input is collected 22
+  
+  
+  var lower = false
+  var result = confirm("Do you want the password to include lowercase charaters?")
+  
+  if (result) {
+  var lower = true
+  includedArrays = includedArrays.concat(lowerCasedCharacters)
+  }
+  console.log(includedArrays) // check the arrays have been added to the included arrays or not depending on user choice 
+  
+  // checking if user wants their password to include Upper case charaters - if the array is added to the incluidedArrays array
+  
+  
+  var upper = false
+  var result = confirm("Do you want the password to include uppercase charaters?")
+  
+  if (result) {
+  var upper = true
+  includedArrays = includedArrays.concat(upperCasedCharacters)
+  }
+  console.log(includedArrays) // check the arrays have been added to the included arrays or not depending on user choice 
+  
+  
+  // checking if user wants their password to include nummeric charaters - if the numeric array is added to the incluidedArrays array
+  var numeric = false
+  var result = confirm("Do you want the password to include numeric charaters?")
+  
+  if (result) {
+  var numeric = true
+   includedArrays= includedArrays.concat(numericCharacters)
+  }
+  console.log(includedArrays) // check the arrays have been added to the included arrays or not depending on user choice 
+  
+  
+  // checking if user wants their password to include special charaters - if so the array is added to the incluidedArrays array
+  
+  
+  var special = false
+  var result = confirm("Do you want the password to special charaters?")
+  
+  if (result) {
+  var special = true
+  includedArrays = includedArrays.concat(specialCharacters)
+  }
+  console.log(includedArrays) // check the arrays have been added to the included arrays or not depending on user choice 
+  
+  console.log(" The final chosen characterset by user is " + includedArrays)
+  }
+  
 
-// Function for getting a random element from an array
-function getRandom(arr) {
 
-}
 
+//----------------------Second function --------------------------------------------------- get random element--------------------------------------------
+
+// Function for getting a random element from an array33
+ function getRandom() {
+    arr =   includedArrays                   //upperCasedCharacters
+    let x = Math.floor(Math.random() * arr.length); // Testing with uppercase array
+    let randomLetter = arr[x]
+    console.log("The array to chose your passwords from is " + arr)
+    console.log("the chosen character " + randomLetter )
+    return randomLetter
+  }
+
+
+  
 // Function to generate password with user input
-function generatePassword() {
 
-}
+function GeneratePssword(funct){
+  var chosenpass = []
+  console.log(chosenpass)
+  
+  for(let i = 0; i < psLength ; i++){
+      
+      x = funct()
+      chosenpass.push(x) 
+      console.log(chosenpass)
+  }
+  Newpass = chosenpass.join("")
+  console.log("Your chosen password is " + Newpass)
+  }
+
+
+//-------------------------prewritten coce - Do not touch-----------------------------------------------------
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -116,3 +201,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+//--------------------------RUN FUNCTIONS-----------------------------
+
+getPasswordOptions();
+GeneratePssword(getRandom)
